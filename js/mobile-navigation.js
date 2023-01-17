790fb3c54fc70159240c557c61ce9a26f49aa53a
Rubical <zhpiv96@mail.ru>
2023-01-17 09:43:09 +0300

    upd

--- js/mobile-navigation.js
+++ js/mobile-navigation.js
@@ -1,19 +1,19 @@
 const overflowWrapper = document.querySelector(".overflow-wrapper");
 const mobileNav = document.querySelector(".modal-box");
 const body = document.querySelector("body");
+const mobileNavOpenBtn = document.querySelector(".nav-open-svg");
+
 
 body.addEventListener("click", function (event) {
-  if (event.target == document.querySelectorAll("#navOpen")) {
-    const mobileNavOpenBtn = document.querySelector(".nav-open-svg");
-
-    overflowWrapper.classList.toggle("blur");
-
-    if (overflowWrapper.classList.contains("blur")) {
-      mobileNavOpenBtn.src = "./image/icons/nav-close.svg";
-      mobileNav.classList.remove("none");
-    } else {
-      mobileNavOpenBtn.src = "./image/icons/nav-open.svg";
+  if (event.target.dataset.action === 'open' ||  event.target.dataset.action === 'close') {
+  overflowWrapper.classList.toggle("blur");
+  } 
+    overflowWrapper.classList.contains("blur") 
+    ?
+      (mobileNavOpenBtn.src = "./image/icons/nav-close.svg",
+      mobileNav.classList.remove("none"))
+      
+    : mobileNavOpenBtn.innerHTML = `img src="./image/icons/nav-close.svg" alt="" class="nav-open-svg`;
       mobileNav.classList.add("none");
     }
-  }
-});
+  );
