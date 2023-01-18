04d414b0e12ba406dc578f1c7c7c225f56c3abe5
Rubical <zhpiv96@mail.ru>
2023-01-18 21:34:53 +0300

    upd

--- js/mobile-navigation.js
+++ js/mobile-navigation.js
@@ -3,22 +3,28 @@ const body = document.body;
 const btnOpen = document.querySelector("[data-action = open]");
 const btnClose = document.querySelector("[data-action = close]");
 
-btnOpen.addEventListener("click", function (event) {
-    const btnOpenCoords = btnOpen.getBoundingClientRect();
-    btnClose.style.position = "fixed";
-    btnClose.style.left = btnOpenCoords.left + "px";
-    mobileNav.classList.remove("none");
-    disableScroll();
+/* Mobile menu functional */
 
+btnOpen.addEventListener("click", function () {
+  showModal();
+  disableScroll();
 });
 
 btnClose.addEventListener("click", function () {
+  hideModal();
+  enableScroll();
+});
+
+function showModal() {
+  const btnOpenCoords = btnOpen.getBoundingClientRect();
+  btnClose.style.position = "fixed";
+  btnClose.style.left = btnOpenCoords.left + "px";
+  mobileNav.classList.remove("none");
+}
+
+function hideModal() {
   mobileNav.classList.add("none");
-    enableScroll();
-  })
-
-
-
+}
 
 function disableScroll() {
   let pagePosition = window.scrollY;
@@ -34,3 +40,25 @@ function enableScroll() {
   window.scroll({ top: pagePosition, left: 0 });
   document.body.removeAttribute("data-position");
 }
+
+/* Mobile menu navi*/
+
+mobileNav.addEventListener("click", function (event) {
+  if (event.target.dataset.location == "cafeMenu") {
+    hideModal();
+    enableScroll();
+    cafeMenu.scrollIntoView({ block: "start", behavior: "smooth" });
+  } else if (event.target.dataset.location == "download") {
+    hideModal();
+    enableScroll();
+    download.scrollIntoView({ block: "center", behavior: "smooth" });
+  } else if (event.target.dataset.location == "reviews") {
+    hideModal();
+    enableScroll();
+    reviews.scrollIntoView({ block: "center", behavior: "smooth" });
+  } else if (event.target.dataset.location == "contacts") {
+    hideModal();
+    enableScroll();
+    contacts.scrollIntoView({ block: "start", behavior: "smooth" });
+  }
+});
---
+++ js/scroll.js
@@ -0,0 +1,18 @@
+const cafeMenu = document.querySelector("#cafeMenu");
+const download = document.querySelector("#download");
+const reviews = document.querySelector("#reviews");
+const contacts = document.querySelector("#contacts");
+
+const navBar = document.querySelector(".navbar-nav");
+
+navBar.addEventListener("click", function (event) {
+  if (event.target.dataset.location == "cafeMenu") {
+    cafeMenu.scrollIntoView({ block: "center", behavior: "smooth" });
+  } else if (event.target.dataset.location == "download") {
+    download.scrollIntoView({ block: "center", behavior: "smooth" });
+  } else if (event.target.dataset.location == "reviews") {
+    reviews.scrollIntoView({ block: "start", behavior: "smooth" });
+  } else if (event.target.dataset.location == "contacts") {
+    contacts.scrollIntoView({ block: "start", behavior: "smooth" });
+  }
+});
